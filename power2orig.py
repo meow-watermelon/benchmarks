50a6c33ee8a3d7d267d32c238df2c8c757755275
Watermelon <herdingcat@yahoo.com>
2018-12-20 22:33:18 -0800

    power2orig - add python version
    Signed-off-by: Watermelon <herdingcat@yahoo.com>

---
+++ power2orig.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+'''
+find Kaprekar numbers
+'''
+
+import time
+
+init = 1000
+start_time = time.time()
+
+while True:
+    if len(str(init)) % 2 != 0:
+        init = init * 10
+
+    splitter = len(str(init)) / 2
+    fore = str(init)[:splitter]
+    post = str(init)[splitter:]
+
+    if (int(fore) + int(post)) ** 2 == init:
+        print('{0} {1} {2} {3:.6f}'.format(fore, post, init, time.time() - start_time))
+
+    init = init + 1
