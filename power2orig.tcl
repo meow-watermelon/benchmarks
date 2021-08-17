b8cca099c1bacd185f9a45f69d4803c70ad4111c
Hui Li <herdingcat@yahoo.com>
2021-08-16 20:56:56 -0700

    always brace the expression
    Signed-off-by: Hui Li <herdingcat@yahoo.com>

--- power2orig.tcl
+++ power2orig.tcl
@@ -4,17 +4,17 @@ set init 1000
 set start_time [clock microseconds]
 
 while {true} {
-    if {[expr [string length $init] % 2] != 0} {
-        set init [expr $init * 10]
+    if {[expr {[string length $init] % 2}] != 0} {
+        set init [expr {$init * 10}]
     }
 
-    set splitter [expr [string length $init] / 2]
+    set splitter [expr {[string length $init] / 2}]
 
     set fore [string range $init 0 $splitter-1]
     set post [string range $init $splitter end]
 
-    if {[expr ([scan $fore %lld] + [scan $post %lld]) ** 2] == $init} {
-        puts "$fore $post $init [expr ([clock microseconds] - $start_time) / 1000000.0]"
+    if {[expr {([scan $fore %lld] + [scan $post %lld]) ** 2}] == $init} {
+        puts "$fore $post $init [expr {([clock microseconds] - $start_time) / 1000000.0}]"
     }
 
     incr init
